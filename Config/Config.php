4fb2fdbc41d41e2120e2285abaced775c688aa9a
marvin712 <62572934+marvin712@users.noreply.github.com>
2023-10-21 21:22:34 -0600

    Update Config.php

--- Config/Config.php
+++ Config/Config.php
@@ -5,10 +5,10 @@
     date_default_timezone_set('America/Guatemala');
 
     //Datos de conexión a Base de Datos
-    const DB_HOST = "localhost";
-    const DB_NAME = "instituto";
-    const DB_USER = "root";
-    const DB_PASSWORD = "root";
+    const DB_HOST = "bdim2qom7xpr1fppo6vr-mysql.services.clever-cloud.com";
+    const DB_NAME = "bdim2qom7xpr1fppo6vr";
+    const DB_USER = "u8z2kotj6eqgnjms";
+    const DB_PASSWORD = "DnZHV4mhmbxnk8d6ZHN6";
     const DB_CHARSET = "charset=utf-8";
 
     //Delimitadores decimal y millar Ej. 24,1989.00
